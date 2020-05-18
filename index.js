c507c76ab7e9f210fe8aad9638d27fdbd48ff482
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-05-18 01:20:17 -0700

    yvette-luong

--- index.js
+++ index.js
@@ -208,9 +208,16 @@ const artists = [
 (1) Name of the first artist in the array
 (2) Bio of the third artist in the array */
 
+console.log(artists[0].name)
 
+console.log(artists[2].bio)
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+
+
+artists[8].name = "Vincent Van Gogh"
+
+console.log(artists[8].name)
 
 
 
@@ -223,9 +230,9 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(id, name) {
-    /* code here */
+    return `The artist at index ${id} is ${name}.`;
   }
-  
+  console.log(getArtistByIndex(artists[0].id, artists[0].name))
   /**
 
 
@@ -237,16 +244,17 @@ function getArtistByIndex(id, name) {
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset.
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(artistArr, index) {
+  artistArr.splice(index, index + 1);    
+    return artistArr
   }
-  
+  console.log(removeArtist(artists, 7))
   /**
 
 
 /* Task 5: Create a function called lotsOfArt() that takes artists as an argument and returns an array with names of artists who painted more than 100 paintings */
 
-function lotsOfArt(/* Code here */){
+function lotsOfArt(name){
 
     /* Code here */
 
